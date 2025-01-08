1a866c5a0ba2399539dafc92332cc9f909b2fc51
Никита <163734198+b3d0la9ich@users.noreply.github.com>
2025-01-08 16:46:19 +0700

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-from app import app  # Импортируем приложение из __init__.py
-
-@app.route("/")
-def index():
-    return render_template("base.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
